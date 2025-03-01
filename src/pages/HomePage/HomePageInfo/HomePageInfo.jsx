3798669da4b60181438262228dd892421cd3a014
CuteBuffy <cutebuffydiscord@gmail.com>
2025-03-01 23:00:21 +0200

    fix

--- src/pages/HomePage/HomePageInfo/HomePageInfo.jsx
+++ src/pages/HomePage/HomePageInfo/HomePageInfo.jsx
@@ -1,6 +1,9 @@
 import BoostifyLogo from "../../../icons/boostify_logo.svg"
 import InstagramLogo from "../../../icons/instagram3d.svg"
 import Branches from "../../../icons/branches.svg"
+import BranchesTest from "../../../test/branches.png"
+import InstagramTest from "../../../test/instagram3d.png"
+import BoostifyTest from "../../../test/logo.png"
 
 const HomePageInfo = () => {
   return (
@@ -8,16 +11,16 @@ const HomePageInfo = () => {
       <div className="main__page_info_wrapper">
         <div className="main__page_info_text">
           <div className="main__page_info_text_logo">
-            <img src={BoostifyLogo} alt="" />
+            <img className="boostify__logo_img" src={BoostifyTest} alt="" />
             <p className="main__page_info_text_boostify">boostify</p>
           </div>
           <p className="main__page_info_text_desc">Накрутка для тебе
             за пару кліків</p>
         </div>
-        <img src={InstagramLogo} alt="" />
+        <img className="instagram__img" src={InstagramTest} alt="" />
       </div>
       <div className="main__page_branches">
-        <img src={Branches} alt="" />
+        <img src={BranchesTest} alt="" />
       </div>
     </div>
   )
